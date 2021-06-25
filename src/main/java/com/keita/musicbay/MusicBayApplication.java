b932e05b424d0e30c49152a63bbfea063ef216c4
Moussa <Moussa.Keita2411@gmail.com>
2021-06-24 22:27:09 -0400

    first commit

---
+++ src/main/java/com/keita/musicbay/MusicBayApplication.java
@@ -0,0 +1,13 @@
+package com.keita.musicbay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicBayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicBayApplication.class, args);
+    }
+
+}
